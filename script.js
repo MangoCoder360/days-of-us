973837be5ec1587405f9db3946ae6319d5a2e953
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2025-07-08 14:12:41 -0700

    added weeks and months, changed font

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 function tick() {
-const startDate = new Date(2025, 4, 13);
+  const startDate = new Date(2025, 4, 13);
   const today = new Date();
   const diffTime = today - startDate;
   const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
   const daysElement = document.getElementById('days');
   daysElement.textContent = diffDays;
+
+  const weeksElement = document.getElementById('weeks');
+  const diffWeeks = Math.floor(diffDays / 7);
+  weeksElement.textContent = diffWeeks + " weeks";
+  const monthsElement = document.getElementById('months');
+  const diffMonths = Math.floor(diffDays / 30);
+  monthsElement.textContent = diffMonths + " months";
 }
 
 document.addEventListener('DOMContentLoaded', () => {
